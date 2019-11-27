54fd16d6d151eaeca248b90f042e991ea46aae00
DaanMoura <daniel.lucio.moura@gmail.com>
2019-11-27 20:30:25 -0300

    tratando localStorage vazio

--- src/pages/saved/index.jsx
+++ src/pages/saved/index.jsx
@@ -1,27 +1,44 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
 import '../../shared/style.css';
 import PokemonsContainer from '../../components/PokemonsContainer';
 
 const Saved = () => {
-    const [pokemons,setPokemons] = useState([]);
+	const [pokemons, setPokemons] = useState([]);
+	const [isEmpty, setIsEmpty] = useState(false);
 
-    useEffect(() => {
+	useEffect(() => {
         const savedPokemonsJson = localStorage.getItem('saved_list');
+        if(!savedPokemonsJson) {
+            setIsEmpty(true);
+            return;
+        }
         const savedPokemons = JSON.parse(savedPokemonsJson);
-        const _pokemons = savedPokemons.map(pokemon => {
-            const pokemonJson = localStorage.getItem(pokemon);
-            return JSON.parse(pokemonJson);
-        });
-        setPokemons(_pokemons);
-    }, []);
+        if(savedPokemons.length < 1) {
+            setIsEmpty(true);
+            return;
+        }
+		const _pokemons = savedPokemons.map(pokemon => {
+			const pokemonJson = localStorage.getItem(pokemon);
+			return JSON.parse(pokemonJson);
+		});
+		setPokemons(_pokemons);
+	}, []);
 
-    return (
-        <div>
-            <h1>Your favorites pokémons</h1>
-            <PokemonsContainer pokemons={pokemons}/>
-        </div>
-    )
-}
+	return (
+		<div>
+			<h1>Your favorites pokémons</h1>
+			{isEmpty ? (
+				<p>
+					Nothing to show here...
+					<br />
+					Use 'Favorite' button to save your favorites pokémons!
+				</p>
+			) : (
+				<PokemonsContainer pokemons={pokemons} />
+			)}
+		</div>
+	);
+};
 
 export default Saved;
